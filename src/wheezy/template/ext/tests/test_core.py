dae7a587147386b4ac4f7294fb3963c74c9ff478
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-06-14 14:51:51 +0300

    Minor refactoring in core extension lexer tests.

--- src/wheezy/template/ext/tests/test_core.py
+++ src/wheezy/template/ext/tests/test_core.py
@@ -31,46 +31,48 @@ class CleanSourceTestCase(unittest.TestCase):
 
 
 class LexerTestCase(unittest.TestCase):
-    """ Test the ``CoreExtension``.
+    """ Test the ``CoreExtension`` lexers.
     """
 
     def setUp(self):
         from wheezy.template.engine import Engine
         from wheezy.template.ext.core import CoreExtension
         self.engine = Engine(extensions=[CoreExtension()])
-        self.lexer = self.engine.lexer
+
+    def tokenize(self, source):
+        return self.engine.lexer.tokenize(source)
 
     def test_stmt_token(self):
         """ Test statement token.
         """
-        tokens = self.lexer.tokenize('@require(title, users)\n')
+        tokens = self.tokenize('@require(title, users)\n')
         assert (1, 'require', 'require(title, users)') == tokens[0]
 
     def test_var_token(self):
         """ Test variable token.
         """
-        tokens = self.lexer.tokenize('@user.name ')
+        tokens = self.tokenize('@user.name ')
         assert (1, 'var', 'user.name') == tokens[0]
-        tokens = self.lexer.tokenize('@user.pref[i].fmt() ')
+        tokens = self.tokenize('@user.pref[i].fmt() ')
         assert (1, 'var', 'user.pref[i].fmt()') == tokens[0]
 
     def test_markup_token(self):
         """ Test markup token.
         """
-        tokens = self.lexer.tokenize(' test ')
+        tokens = self.tokenize(' test ')
         assert 1 == len(tokens)
         assert (1, 'markup', ' test ') == tokens[0]
 
     def test_markup_token_escape(self):
         """ Test markup token with escape.
         """
-        tokens = self.lexer.tokenize('support@@acme.org')
+        tokens = self.tokenize('support@@acme.org')
         assert 1 == len(tokens)
         assert (1, 'markup', 'support@acme.org') == tokens[0]
 
 
 class ParserTestCase(unittest.TestCase):
-    """ Test the ``CoreExtension``.
+    """ Test the ``CoreExtension`` parsers.
     """
 
     def setUp(self):
