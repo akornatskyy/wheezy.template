3f9068ba383931bbb6cc47d93c9bd9815a3f4bb8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-06-21 15:06:05 +0300

    Added unit tests for DictLoader.

--- src/wheezy/template/tests/test_loader.py
+++ src/wheezy/template/tests/test_loader.py
@@ -27,14 +27,43 @@ class FileLoaderTestCase(unittest.TestCase):
     def test_load_existing(self):
         """ Tests load.
         """
-        assert self.loader.load('tmpl1.html')
+        assert '' == self.loader.load('tmpl1.html')
 
     def test_load_not_found(self):
         """ Tests load if the name is not found.
         """
-        assert not self.loader.load('tmpl-x.html')
+        assert None == self.loader.load('tmpl-x.html')
 
     def test_load_not_a_file(self):
         """ Tests load if the name is not a file.
         """
         assert not self.loader.load('shared/snippet')
+
+
+class DictLoaderTestCase(unittest.TestCase):
+    """ Test the ``DictLoader``.
+    """
+
+    def setUp(self):
+        from wheezy.template.loader import DictLoader
+        self.loader = DictLoader(templates={
+                'tmpl1.html': 'x',
+                'shared/master.html': 'x'
+        })
+
+    def test_list_names(self):
+        """ Tests list_names.
+        """
+        assert ['tmpl1.html',
+                'shared/master.html'
+        ] == self.loader.list_names()
+
+    def test_load_existing(self):
+        """ Tests load.
+        """
+        assert 'x' == self.loader.load('tmpl1.html')
+
+    def test_load_not_found(self):
+        """ Tests load if the name is not found.
+        """
+        assert None == self.loader.load('tmpl-x.html')
