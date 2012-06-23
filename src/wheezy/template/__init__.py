c069c55a05bf262b02bafc232c4085f895506818
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-06-23 18:16:15 +0300

    Added top level package imports.

---
+++ src/wheezy/template/__init__.py
@@ -0,0 +1,4 @@
+from wheezy.template.engine import Engine
+from wheezy.template.loader import FileLoader
+from wheezy.template.loader import DictLoader
+from wheezy.template.ext.core import CoreExtension
