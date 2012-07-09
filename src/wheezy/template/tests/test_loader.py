c2cd8b52316dea047371c0e5723e179ccc295984
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-07-09 14:09:49 +0300

    Added unit tests for ChainLoader.

--- src/wheezy/template/tests/test_loader.py
+++ src/wheezy/template/tests/test_loader.py
@@ -69,3 +69,38 @@ class DictLoaderTestCase(unittest.TestCase):
         """ Tests load if the name is not found.
         """
         assert None == self.loader.load('tmpl-x.html')
+
+
+class ChainLoaderTestCase(unittest.TestCase):
+    """ Test the ``ChainLoader``.
+    """
+
+    def setUp(self):
+        from wheezy.template.loader import ChainLoader
+        from wheezy.template.loader import DictLoader
+        self.loader = ChainLoader(loaders=[
+            DictLoader(templates={
+                'tmpl1.html': 'x1',
+            }),
+            DictLoader(templates={
+                'shared/master.html': 'x2'
+            })])
+
+    def test_list_names(self):
+        """ Tests list_names.
+        """
+        assert (
+            'tmpl1.html',
+            'shared/master.html'
+        ) == self.loader.list_names()
+
+    def test_load_existing(self):
+        """ Tests load.
+        """
+        assert 'x1' == self.loader.load('tmpl1.html')
+        assert 'x2' == self.loader.load('shared/master.html')
+
+    def test_load_not_found(self):
+        """ Tests load if the name is not found.
+        """
+        assert None == self.loader.load('tmpl-x.html')
