52a079ab42465cf087b2a6dec45b4cb2fe6c6186
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-12-18 14:00:57 +0200

    Improved test cases to cover preprocessors, postprocessors and extension lexer scan.

--- src/wheezy/template/tests/test_lexer.py
+++ src/wheezy/template/tests/test_lexer.py
@@ -14,6 +14,7 @@ class LexerTestCase(unittest.TestCase):
         """
         import re
         from wheezy.template.lexer import Lexer
+        from wheezy.template.lexer import lexer_scan
 
         def word_token(m):
             return m.end(), 'w', m.group()
@@ -21,13 +22,27 @@ class LexerTestCase(unittest.TestCase):
         def blank_token(m):
             return m.end(), 'b', m.group()
 
-        lexer = Lexer([
-            (re.compile(r'\w+'), word_token),
-            (re.compile(r'\s+'), blank_token),
-        ])
-        assert [(1, 'w', 'hello'),
-                (1, 'b', '\n '),
-                (2, 'w', 'world')] == lexer.tokenize('hello\n world')
+        def to_upper(s):
+            return s.upper()
+
+        def cleanup(tokens):
+            for i in range(len(tokens)):
+                t = tokens[i]
+                if t[i] == 'b':
+                    tokens[i] = (t[0], 'b', ' ')
+
+        class Extension(object):
+            lexer_rules = {
+                100: (re.compile(r'\w+'), word_token),
+                200: (re.compile(r'\s+'), blank_token)
+            }
+            preprocessors = [to_upper]
+            postprocessors = [cleanup]
+
+        lexer = Lexer(**lexer_scan([Extension]))
+        assert [(1, 'w', 'HELLO'),
+                (1, 'b', ' '),
+                (2, 'w', 'WORLD')] == lexer.tokenize('hello\n world')
 
     def test_trivial(self):
         """ Empty rules and source
