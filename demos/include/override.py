b65b70b2927974790ff2413843ec752dd9c6c609
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-05-11 11:24:16 +0300

    Added demo for include override.

---
+++ demos/include/override.py
@@ -0,0 +1,75 @@
+""" Demo: overrides `include` directive.
+
+    Note: an alternative implementation for `render` demo.
+"""
+
+import unittest
+
+from wheezy.template.engine import Engine
+from wheezy.template.ext.core import CoreExtension
+from wheezy.template.loader import DictLoader
+
+
+master = """
+@def content():
+@end
+
+@content()
+"""
+
+page_a = """
+@extends('master')
+
+@def content():
+  a
+@end
+"""
+
+page_b = """
+@extends('master')
+
+@def content():
+  b
+@end
+"""
+
+page_all = """
+@include('page_a')
+@include('page_b')
+"""
+
+pages = {
+    'master': master,
+    'page_a': page_a,
+    'page_b': page_b,
+    'page_all': page_all
+}
+
+
+def build_include(builder, lineno, token, value):
+    assert token == 'include'
+    builder.add(lineno, 'w(_r(' + value + ', ctx, {}, {}))')
+    return True
+
+
+class MyExtention(object):
+
+    builder_rules = [('include', build_include)]
+
+
+engine = Engine(
+    loader=DictLoader(pages),
+    extensions=[MyExtention(), CoreExtension()]
+)
+
+
+class TestCase(unittest.TestCase):
+
+    def test_render(self):
+        template = engine.get_template('page_all')
+        r = template.render({})
+        self.assertEqual(['a', 'b'], r.split())
+
+
+if __name__ == '__main__':
+    unittest.main()
