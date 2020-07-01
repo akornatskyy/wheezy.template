45acb1e9f01170bd97bac37bbfc3317afd91a138
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2020-07-01 20:38:55 +0300

    Fixed package level imports.

--- src/wheezy/template/__init__.py
+++ src/wheezy/template/__init__.py
@@ -1,7 +1,20 @@
 """
 """
 
-# flake8: noqa
+from wheezy.template.engine import Engine
+from wheezy.template.ext.code import CodeExtension
+from wheezy.template.ext.core import CoreExtension
+from wheezy.template.loader import DictLoader, FileLoader, PreprocessLoader
+from wheezy.template.preprocessor import Preprocessor
 
+__all__ = (
+    "Engine",
+    "CodeExtension",
+    "CoreExtension",
+    "DictLoader",
+    "FileLoader",
+    "PreprocessLoader",
+    "Preprocessor",
+)
 
 __version__ = "0.1"
