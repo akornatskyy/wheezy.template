e6e889ef6e9104195c67f1a848bc0d4fd6f80ed0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2020-12-01 20:41:08 +0200

    Fixed issue #23, installation hangs on Windows with cython nthreads>=1.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import multiprocessing
 import os
 import re
 
@@ -16,8 +17,9 @@ try:
             os.path.join(p, "__init__.py"),
             os.path.join(p, "ext", "__init__.py"),
         ],
-        nthreads=2,
-        compiler_directives = {'language_level': 3},
+        # https://github.com/cython/cython/issues/3262
+        nthreads=0 if multiprocessing.get_start_method() == "spawn" else 2,
+        compiler_directives={"language_level": 3},
         quiet=True,
     )
 except ImportError:
